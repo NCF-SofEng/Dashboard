b304b09dc6f9cfe34c28b78413563bde12079f0e
Devon Gardner <dgardner365@gmail.com>
2022-05-13 02:17:27 -0400

    made some slight styling changes to youtube

--- src/components/YoutubeBody.js
+++ src/components/YoutubeBody.js
@@ -1,3 +1,4 @@
+import "../styles/YoutubeBody.css";
 import { useEffect, useState } from "react";
 
 const YoutubeBody = () => {
@@ -5,6 +6,7 @@ const YoutubeBody = () => {
     const [videos, setVideos] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
     const [error, setError]  = useState(null);
+
     useEffect(() => {
             fetch("https://rvhcdjwc8e.execute-api.us-east-1.amazonaws.com/api/youtube/videos")
                 .then(response => response.json())
@@ -16,24 +18,34 @@ const YoutubeBody = () => {
                     console.error(error);
                     setError(error);
                 });
-        },[]
-    );
+        },[]);
+
     const updateVideo = (integer) => {
         if (currentVideo + integer >= 0 && currentVideo + integer < videos.length){
             setCurrentVideo(currentVideo + integer);
         }
     }
-    return (
-        <div>
-            <div onClick={() => updateVideo(1)}>
-                Next 
-            </div>
-            {isLoaded ? 
-            <iframe className="YoutubeVideoIframe" src={"https://www.youtube.com/embed/" + videos[currentVideo].id.videoId} title="YouTube video player" allowFullScreen></iframe>
-            :"Loading"}<div onClick={() => updateVideo(-1)}>
-                Prev
-            </div>
-    </div>);
+
+    if (isLoaded) {
+        if (!error) {
+            return (
+                <div>
+                    <div className="YoutubeCurrentVideo">Current Video: {currentVideo + 1}/{videos.length}</div>
+                    <div className="YoutubeButton" onClick={() => updateVideo(1)}>
+                        Next
+                    </div>
+                    <iframe className="YoutubeVideoIframe" src={"https://www.youtube.com/embed/" + videos[currentVideo].id.videoId} title="YouTube video player" allowFullScreen></iframe>
+                    <div className="YoutubeButton" onClick={() => updateVideo(-1)}>
+                        Prev
+                    </div>
+                </div>);
+        } else {
+            return <div className="Error">{error}</div>;
+        }
+    } else {
+        return <div className="Loading">Youtube videos loading...</div>;
+    }
+
 }
 
 export default YoutubeBody

101c7a059bb498cc1a9d84fd947b58835e3a730e
Devon Gardner <dgardner365@gmail.com>
2022-05-12 23:29:17 -0400

    updated YouTube fetch url

--- src/components/youtube/YoutubeBody.js
+++ src/components/youtube/YoutubeBody.js
@@ -1,26 +1,26 @@
 import { useEffect, useState } from "react";
 
 const YoutubeBody = () => {
-  const [videos, setVideos] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [error, setError]  = useState(null);  
-  useEffect(() => {
-    fetch("https://rvhcdjwc8e.execute-api.us-east-1.amazonaws.com/api/videos")
-    .then(response => response.json())
-    .then(response => {
-      setIsLoaded(true);
-      setVideos(response.data);
-      console.log(response.data);
-    })
-    .catch(error => {
-        console.error(error);
-        setError(error);
-    });
-  },[]
-  );
-  return (
-        <iframe className="YoutubeVideoIframe" src={"https://www.youtube.com/embed/riEQ8KpcXRQ"} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      );
+    const [videos, setVideos] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError]  = useState(null);
+    useEffect(() => {
+            fetch("https://rvhcdjwc8e.execute-api.us-east-1.amazonaws.com/api/youtube/videos")
+                .then(response => response.json())
+                .then(response => {
+                    setIsLoaded(true);
+                    setVideos(response.data);
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.error(error);
+                    setError(error);
+                });
+        },[]
+    );
+    return (
+        <iframe className="YoutubeVideoIframe" src={"https://www.youtube.com/embed/riEQ8KpcXRQ"} title="YouTube video player" allowFullScreen></iframe>
+    );
 }
 
 export default YoutubeBody

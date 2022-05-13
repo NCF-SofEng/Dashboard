ade8b825bb437f10352bb6a1bfdc2eb0643dc600
Devon Gardner <dgardner365@gmail.com>
2022-05-13 02:19:56 -0400

    enabled dynamic iframe generation from spotify database collection

--- src/components/SpotifyBody.js
+++ src/components/SpotifyBody.js
@@ -1,14 +1,44 @@
 import "../styles/SpotifyBody.css";
+import {useEffect, useState} from "react";
 
 const SpotifyBody = () => {
-  return (
-    <div className="SpotifyBody">
-      <iframe style={{border: 0, borderRadius: "12px"}} title="SpotifyPlaylist"
-              src="https://open.spotify.com/embed/playlist/33PAH14e83xTvAsv4irStG?utm_source=generator&theme=0"
-              width="100%" height="1225" allowFullScreen=""
-              allow="fullscreen"/>
-    </div>
-  );
+    const [episodes, setEpisodes] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://rvhcdjwc8e.execute-api.us-east-1.amazonaws.com/api/spotify/episodes")
+            .then(response => response.json())
+            .then(response => {
+                setEpisodes(response.data);
+                setIsLoaded(true);
+            })
+            .catch(err => {
+                console.error(err);
+                setError(err);
+            });
+    }, [])
+
+    if (!isLoaded) {
+        return <div className="Loading">Loading episodes...</div>;
+    } else {
+        if (error) {
+            return <div className="Error">{error}</div>;
+        } else {
+            return (
+                <div className="SpotifyBody">
+                    {
+                        episodes.map((episode, idx) => {
+                            return (<iframe style={{borderRadius: "12px"}} title={`SpotifyEpisode${idx}`} key={idx}
+                            src={`https://open.spotify.com/embed/episode/${episode.uri.split(":").slice(-1)}?utm_source=generator`}
+                            width="100%" height="232" allowFullScreen=""
+                            allow="fullscreen"/>);
+                        })
+                    }
+                </div>
+            );
+        }
+    }
 }
 
 export default SpotifyBody;
